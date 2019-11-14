f7d67bb63898c2a5cd347f19eb92687c999fb4e4
DeAndre Johnson <dre@phreshr.com>
2019-11-14 12:56:22 +0800

    update

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,31 +3,36 @@ import style from './style'
 
 import Project from '../Project'
 
+import bookstoreImg from '../../images/react-bookstore.png'
+import nasaImg from '../../images/nasa-photo-of-the-day.iamdrejohnson.now.sh_.png'
+import lambdaTimesImg from '../../images/lambda-times.png'
+import calcImg from '../../images/lambda-calculator.png'
+
 const projects = [
   {
     title: 'Book Store Shopping Cart',
-    image: '/assets/images/react-bookstore.png',
+    image: `${bookstoreImg}`,
     text:
       'Built out the functionality of a shopping. Pulled book data from an API using Axios, Used the React Context API store data/state globally',
     link: 'https://github.com/drejohnson/react-shopping-cart',
   },
   {
     title: 'NASA APOD Project',
-    image: '/assets/images/nasa-photo-of-the-day.iamdrejohnson.now.sh_.png',
+    image: `${nasaImg}`,
     text:
       'Used Axios to fetch the "Astronomy Photo Of The Day from the NASA API and React and Styled-Components for the UI"',
     link: 'https://github.com/drejohnson/nasa-photo-of-the-day',
   },
   {
     title: 'Lambda Newspaper Project',
-    image: '/assets/images/lambda-times.png',
+    image: `${lambdaTimesImg}`,
     text:
       'Create this project using the DOM, DOM Events, functional Components, and data request by uing the axios library. Also built a fully functional carousel from scratch.',
     link: 'https://github.com/drejohnson/nasa-photo-of-the-day',
   },
   {
     title: 'Lambda Calculator',
-    image: '/assets/images/lambda-calculator.png',
+    image: `${calcImg}`,
     text: 'Used React to built out a fully functional calculator',
     link: 'https://github.com/drejohnson/lambda-calculator',
   },
